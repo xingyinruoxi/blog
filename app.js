cada2132a58f10f33444c735d887f9c846e78dee
周林 <634023699@qq.com>
2018-12-29 09:11:27 +0800

    添加前后台路由配置

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-var express=require('express');
+var express = require('express');
 
-var app=express();
+var app = express();
 
-var swig=require('swig');
+var swig = require('swig');
 
-app.use('/public',express.static(__dirname+'/public'));
+app.use('/public', express.static(__dirname + '/public'));//静态文件托管
 
-app.engine('html',swig.renderFile);
-app.set('views','./views');
-app.set('view engine','html');
+app.engine('html', swig.renderFile);
+app.set('views', './views');
+app.set('view engine', 'html');
 swig.setDefaults({
-    cache:false
+    cache: false
 });
-app.get('/',function (req,res,next) {
+
+app.use('/admin', require('./routers/admin'));
+app.use('/api', require('./routers/api'));
+app.use('/', require('./routers/main'));
+
+/*app.get('/',function (req,res,next) {
     // res.send('<h1>hello world!!!!</h1>')
     res.render('index')
-});
+});*/
 
-app.listen(8888);
+app.listen(8888, () => console.log('服务启动成功，端口为：8888!'));
---
+++ routers/admin.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+router.get('/user', function (req, res, next) {
+    res.send('<h1>admin user</h1>')
+})
+module.exports=router;
