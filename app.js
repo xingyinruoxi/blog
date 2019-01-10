7b71f679beac0669826710621a57ed01772eba91
周林 <634023699@qq.com>
2019-01-10 13:33:17 +0800

    完善登录注册前后台业务逻辑

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 var swig = require('swig');
 
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 app.use('/public', express.static(__dirname + '/public')); //静态文件托管
 
@@ -14,16 +15,20 @@ app.set('view engine', 'html');
 swig.setDefaults({
     cache: false
 });
+//bodyParser设置
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.use('/admin', require('./routers/admin'));
 app.use('/api', require('./routers/api'));
 app.use('/', require('./routers/main'));
 
-app.get('/',function (req,res,next) {
+app.get('/', function (req, res, next) {
     // res.send('<h1>hello world!!!!</h1>')
     res.render('main/index')
 });
-mongoose.connect('mongodb://localhost:27017/blog', function (err) {
+mongoose.connect('mongodb://localhost:27017/blog',{useNewUrlParser:true}, function (err) {
     if (err) {
         console.log('数据库连接失败');
     } else {
---
+++ public/js/main.js
@@ -0,0 +1,73 @@
+$(function() {
+	var $loginBox = $('#loginBox'),
+        $registerBox = $('#registerBox'),
+        $userInfo=$('#userInfo');
+	//切换登录
+	$registerBox.find('.colMint').on('click', function() {
+		$loginBox.show();
+		$registerBox.hide();
+	});
+	//切换注册
+	$loginBox.find('.colMint').on('click', function() {
+		$loginBox.hide();
+		$registerBox.show();
+    });
+    //注册
+    $registerBox.find('button').on('click',function(){
+        // console.log('ertyui')
+        var username=$registerBox.find('[name="username"]').val();
+        var password=$registerBox.find('[name="password"]').val();
+        var repassword=$registerBox.find('[name="repassword"]').val();
+        // console.log(username,password,repassword)
+        $.ajax({
+            type: 'post',
+            url: '/api/user/register',
+            data: {
+                username: username,
+                password: password,
+                repassword:repassword
+            },
+            dataType: 'json',
+            success:function(data){
+                $registerBox.find('.colWarning').html(data.message);
+                if (!data.code) {
+                    //注册成功
+                    setTimeout(function() {
+                        $loginBox.show();
+		                $registerBox.hide();
+                    }, 1000);
+                }
+            }
+        })
+    })
+
+    //登录
+    $loginBox.find('button').on('click', function() {
+        console.log('login click')
+        //通过ajax提交请求
+        $.ajax({
+            type: 'post',
+            url: '/api/user/login',
+            data: {
+                username: $loginBox.find('[name="username"]').val(),
+                password: $loginBox.find('[name="password"]').val()
+            },
+            dataType: 'json',
+            success: function(result) {
+                console.log('result',result)
+                $loginBox.find('.colWarning').html(result.message);
+            
+                if (!result.code) {
+                    //登录成功
+                    // window.location.reload();
+                    setTimeout(function() {
+                        $userInfo.show();
+		                $loginBox.hide();
+                    }, 1000);
+                }
+            }
+        })
+    })
+
+
+});
